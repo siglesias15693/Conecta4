bf6893aae026d6a35f02fb00d4b7419bc9b6ba2a
Samuel_Iglesias_Iglesias <siglesias15693@alumnes.iesmanacor.cat>
2022-03-31 12:10:01 +0200

    "tercer commit"

--- src/com/company/Conecta4.java
+++ src/com/company/Conecta4.java
@@ -4,54 +4,58 @@ import java.util.Scanner;
 
 public class Conecta4 {
 
-    //cambiando los valores de la matriz "tablero" se puede cambiar el tamaño del tablero
-    public static String[][] tablero = new String[7][7];
-    public static int capacidadColumnas;
-    public static int capacidadFilas;
+    public static String[][] tablero = new String[9][7];
+    public static int Columnas;
+    public static int Filas;
+    public static int jugadasMaximas;
     public static boolean finJuego = false;
 
-    public void mostrarTablero(String[][] tabla) {
-        System.out.print("          ");
-        for (int i = 1; i < capacidadColumnas + 1; i += 1) {
-            System.out.print(i + "   ");
-        }
-        System.out.println("");
+    public static void mostrarTablero(String[][] tabla) {
         System.out.print("         ");
-        for (int i = 0; i < capacidadColumnas; i += 1) {
+        for (int i = 0; i < Columnas; i++) {
             System.out.print("____");
-        }
-        System.out.println("");
-        for (int i = 0; i < capacidadFilas; i++) {
-            System.out.print("       ");
-            for (int j = 0; j < capacidadFilas; j++) {
-                System.out.print(" | " + tabla[i][j]);
+        }System.out.println("");
+
+        for (int i = 0; i < Filas; i++) {
+            System.out.print("        | ");
+            for (int j = 0; j < Columnas; j++) {
+                System.out.print(tabla[j][i]+" | ");
             }
-            System.out.println(" |");
-            if (i < capacidadFilas - 1) {
+            System.out.println();
+            if (i < Filas -1) {
                 System.out.print("        |");
-                for (int p = 1; p < capacidadColumnas - 1; p += 1) {
-                    System.out.print("––––––");
+                for (int p = 0; p < Columnas ; p++) {
+                    if (p==Columnas-1){
+                        System.out.print("-––");
+                    }else{
+                        System.out.print("-–– ");
+                    }
                 }
                 System.out.println("|");
             }
         }
         System.out.print("         ");
-        for (int i = 1; i < capacidadColumnas + 1; i += 1) {
-            System.out.print("¯¯¯¯");
+        for (int i = 1; i < Columnas + 1; i += 1) {
+            System.out.print("¯¯¯ ");
         }
         System.out.println("");
+
+        System.out.print("          ");
+        for (int i = 1; i < Columnas + 1; i += 1) {
+            System.out.print(i + "   ");
+        }System.out.println();
     }
 
-    public void mostrarInterfazTablero(String jugador1, String caracter1, String jugador2, String caracter2, int jugadasMaximas) {
+    public static void mostrarInterfazTablero(String jugador1, String caracter1, String jugador2, String caracter2, int jugadasMaximas) {
         System.out.println("=================================================================");
         System.out.println(jugador1 + " : " + caracter1 + "      " + jugador2 + " : " + caracter2);
         System.out.print("==============================================||| JUGADA N°" + jugadasMaximas + " |||");
     }
 
-    public void verificadorGanador(String auxiliarJugador, String auxilarCaracter) {
+    public static void verificadorGanador(String auxiliarJugador, String auxilarCaracter) {
         //verificacion ganador (horizontal)
-        for (int i = 1; i < capacidadFilas; i += 1) {
-            for (int j = 0; j < capacidadColumnas - 3; j += 1) {
+        for (int i = 1; i < Filas; i += 1) {
+            for (int j = 0; j < Columnas - 3; j += 1) {
                 if (tablero[i][j].equals(auxilarCaracter) && tablero[i][j + 1].equals(auxilarCaracter) && tablero[i][j + 2].equals(auxilarCaracter) && tablero[i][j + 3].equals(auxilarCaracter)) {
                     finJuego = true;
                     mostrarTablero(tablero);
@@ -61,8 +65,8 @@ public class Conecta4 {
         }
 
         //verificacion ganador (vertical)
-        for (int i = 0; i < capacidadFilas; i += 1) {
-            for (int j = 0; j < capacidadColumnas - 3; j += 1) {
+        for (int i = 0; i < Filas; i += 1) {
+            for (int j = 0; j < Columnas - 3; j += 1) {
                 if (tablero[j][i].equals(auxilarCaracter) && tablero[j + 1][i].equals(auxilarCaracter) && tablero[j + 2][i].equals(auxilarCaracter) && tablero[j + 3][i].equals(auxilarCaracter)) {
                     finJuego = true;
                     mostrarTablero(tablero);
@@ -72,8 +76,8 @@ public class Conecta4 {
         }
 
         //verificacion ganador (diagonal)
-        for (int i = 0; i < capacidadColumnas - 4 + 1; i += 1) {
-            for (int j = 0; j < capacidadFilas - 4 + 1; j += 1) {
+        for (int i = 0; i < Columnas - 4 + 1; i += 1) {
+            for (int j = 0; j < Filas - 4 + 1; j += 1) {
                 if (tablero[j][i].equals(auxilarCaracter) && tablero[j + 1][i + 1].equals(auxilarCaracter) && tablero[j + 2][i + 2].equals(auxilarCaracter) && tablero[j + 3][i + 3].equals(auxilarCaracter)) {
                     finJuego = true;
                     mostrarTablero(tablero);
@@ -81,8 +85,8 @@ public class Conecta4 {
                 }
             }
         }
-        for (int i = capacidadColumnas; i > 3; i -= 1) {
-            for (int j = 0; j < capacidadFilas - 3; j += 1) {
+        for (int i = Filas; i > 3; i -= 1) {
+            for (int j = 0; j < Columnas - 3; j += 1) {
                 if (tablero[j][i - 1].equals(auxilarCaracter) && tablero[j + 1][i - 2].equals(auxilarCaracter) && tablero[j + 2][i - 3].equals(auxilarCaracter) && tablero[j + 3][i - 4].equals(auxilarCaracter)) {
                     finJuego = true;
                     mostrarTablero(tablero);
@@ -93,12 +97,9 @@ public class Conecta4 {
     }
 
     public static void main(String[] args) {
-        Conecta4 programa = new Conecta4();
         Scanner lector = new Scanner(System.in);
-        int jugadasMaximas = 1;
-        int columna = 0;
-        int contador = 0;
-        int modoDeJuego = 0;
+        int columna;
+        int contador;
         String mensajeError = "";
         String jugador1;
         String caracter1;
@@ -106,122 +107,116 @@ public class Conecta4 {
         String caracter2;
         boolean verificador = false;
         boolean verificador2 = false;
-        capacidadColumnas = tablero[1].length;
-        capacidadFilas = tablero.length;
-        String auxiliarJugador = "";
-        String auxiliarCaracter = "";
-
-        //titulo
-        modoDeJuego = 2;
+        Columnas = tablero.length;
+        Filas = tablero[0].length;
+        jugadasMaximas = 0;
 
         //Se asigna un caracter por defecto al tablero
-        for (int i = 0; i <= capacidadColumnas - 1; i++) {
-            for (int j = 0; j <= capacidadFilas - 1; j++) {
+        for (int i = 0; i <= Columnas - 1; i++) {
+            for (int j = 0; j <= Filas - 1; j++) {
                 tablero[i][j] = " ";
             }
         }
 
-        verificador = false;
 
-        if (modoDeJuego == 2) {
+        //aplico color azul al nombre y caracter del jugador 1
+        jugador1 = "\033[32m" + "Jugador 1" + "\u001B[0m";
+        caracter1 = "\033[31m" + "O" + "\u001B[0m";
 
-            //aplico color azul al nombre y caracter del jugador 1
-            jugador1 = "\033[32m" + "Jugador 1" + "\u001B[0m";
-            caracter1 = "\033[31m" + "X" + "\u001B[0m";
+        jugador2 = "\033[33mComputadorNoob\u001B[0m";
+        caracter2 = "\033[33mX\u001B[0m";
+        while (!finJuego) {
 
-            jugador2 = "\033[33mComputadorNoob\u001B[0m";
-            caracter2 = "\033[33m§\u001B[0m";
-            while (!finJuego) {
-
-                //turno jugador 1
-                while (!verificador) {
-                    //llamo a la funcion "mostrarTablero" y "mostrarInterfazTablero" de la clase conecta 4;
-                    programa.mostrarTablero(tablero);
-                    programa.mostrarInterfazTablero(jugador1, caracter1, jugador2, caracter2, jugadasMaximas);
-                    System.out.println(mensajeError);
-                    mensajeError = "";
-                    try {
-                        System.out.println(jugador1 + " escriba el numero de columna para poner su ficha:");
-                        String auxiliar = lector.nextLine();
-                        columna = Integer.parseInt(auxiliar) - 1;
-                        if (columna > capacidadColumnas - 1 || columna < 0) {
-                            mensajeError = "\n\033[35m**ERROR:\u001B[0m Debe ingresar un numero entre el 1 al " + capacidadColumnas;
-                        } else {
-                            contador = 1;
-                            for (int i = capacidadColumnas - 1; i > -1; i = i - 1) {
-                                if (!verificador2) {
-                                    //Si se cumple esta condicion, termina el turno del jugador 1
-                                    if (tablero[i][columna] != caracter1 && tablero[i][columna] != caracter2) {
-                                        tablero[i][columna] = caracter1;
-                                        verificador2 = true;
-                                        verificador = true;
-                                        jugadasMaximas++;
-                                    } else {
-                                        contador += 1;
-                                    }
-                                }
-                                if (contador == capacidadFilas + 1) {
-                                    mensajeError = "\n\033[35m**ERROR:\u001B[0m Esta columna esta completa, escoja otra";
+            //turno jugador
+            while (!verificador) {
+                //llamo a la funcion "mostrarTablero" y "mostrarInterfazTablero"
+                mostrarTablero(tablero);
+                mostrarInterfazTablero(jugador1, caracter1, jugador2, caracter2, jugadasMaximas);
+                System.out.println(mensajeError);
+                mensajeError = "";
+                try {
+                    System.out.println(jugador1 + " escriba el numero de columna para poner su ficha:");
+                    String auxiliar = lector.nextLine();
+                    columna = Integer.parseInt(auxiliar);
+                    System.out.println(columna+" "+Columnas);
+                    if (columna > Columnas || columna < 1) {
+                        mensajeError = "\n\033[35m**ERROR:\u001B[0m Debe ingresar un numero entre el 1 al " + Columnas;
+                    } else {
+                        contador = 0;
+                        columna-=1;
+                        for (int i = Filas - 1; i >=0; i--) {
+                            if (!verificador2) {
+                                //Si se cumple esta condicion, termina el turno del jugador 1
+                                if (tablero[columna][i] != caracter1 && tablero[columna][i] != caracter2) {
+                                    tablero[columna][i] = caracter1;
+                                    verificador2 = true;
+                                    verificador = true;
+                                    jugadasMaximas++;
+                                } else {
+                                    contador += 1;
                                 }
                             }
+                            if (contador == Filas) {
+                                mensajeError = "\n\033[35m**ERROR:\u001B[0m Esta columna esta completa, escoja otra";
+                            }
                         }
-                    } catch (NumberFormatException e) {
-                        mensajeError = "\n\033[35m**ERROR:\u001B[0m Ingreso un caracter no valido, debe ingresar un NUMERO";
-                    } catch (Exception e) {
-                        mensajeError = "\n\033[35m**ERROR:\u001B[0m Debe ingresar un numero entre el 1 al " + capacidadColumnas;
+                    }
+                } catch (NumberFormatException e) {
+                    mensajeError = "\n\033[35m**ERROR:\u001B[0m Ingreso un caracter no valido, debe ingresar un NUMERO";
+                } catch (Exception e) {
+                    mensajeError = "\n\033[35m**ERROR:\u001B[0m Debe ingresar un numero entre el 1 al " + Columnas;
+                }
+            }
+            verificador2 = false;
+            verificador = false;
+            //llamo a la funcion "verificarGanador" para controlar si gano el jugador 1
+            verificadorGanador(jugador1, caracter1);
+
+            if (jugadasMaximas == (Filas * Columnas) + 1) {
+                finJuego = true;
+                mostrarTablero(tablero);
+                System.out.println("======================");
+                System.out.println("\033[35m¡¡¡EMPATE!!!\u001B[0m");
+                System.out.println("El tablero esta lleno");
+                System.out.println("======================");
+            }
+
+            //Si el jugador 1 no gano en su turno continua con el ordenador
+            if (!finJuego) {
+
+                //turno ordenador
+                while (!verificador) {
+                    columna = (int) (Math.random() * Columnas);
+                    for (int i = Filas - 1; i > -1; i--) {
+                        if (!verificador2) {
+                            //Si se cumple esta condicion, termina el turno del jugador 2
+                            if (tablero[columna][i] != caracter2 && tablero[columna][i] != caracter1) {
+                                tablero[columna][i] = caracter2;
+                                verificador2 = true;
+                                verificador = true;
+                                jugadasMaximas++;
+                            }
+                        }
                     }
                 }
                 verificador2 = false;
                 verificador = false;
-                auxiliarJugador = jugador1;
-                auxiliarJugador = caracter1;
-                //llamo a la funcion "verificarGanador" para controlar si gano el jugador 1
-                programa.verificadorGanador(jugador1, caracter1);
+                //llamo a la funcion "verificarGanador" para controlar si gano el jugador 2
+                verificadorGanador(jugador2, caracter2);
 
-                if (jugadasMaximas == (capacidadFilas * capacidadColumnas) + 1) {
-                    finJuego = true;
-                    programa.mostrarTablero(tablero);
-                    System.out.println("======================");
-                    System.out.println("\033[35m¡¡¡EMPATE!!!\u001B[0m");
-                    System.out.println("El tablero esta lleno");
-                    System.out.println("======================");
-                }
-
-                //Si el jugador 1 no gano en su turno continua con el ordenador
-                if (finJuego == false) {
-
-                    //turno ordenador
-                    while (!verificador) {
-                        columna = (int) (Math.random() * capacidadColumnas);
-                        for (int i = capacidadColumnas - 1; i > -1; i = i - 1) {
-                            if (!verificador2) {
-                                //Si se cumple esta condicion, termina el turno del jugador 2
-                                if (tablero[i][columna] != caracter2 && tablero[i][columna] != caracter1) {
-                                    tablero[i][columna] = caracter2;
-                                    verificador2 = true;
-                                    verificador = true;
-                                    jugadasMaximas++;
-                                }
-                            }
-                        }
-                    }
-                    verificador2 = false;
-                    verificador = false;
-                    auxiliarJugador = jugador2;
-                    auxiliarJugador = caracter2;
-                    //llamo a la funcion "verificarGanador" para controlar si gano el jugador 2
-                    programa.verificadorGanador(jugador2, caracter2);
-
-                    if (jugadasMaximas == (capacidadFilas * capacidadColumnas) + 1) {
-                        finJuego = true;
-                        programa.mostrarTablero(tablero);
-                        System.out.println("======================");
-                        System.out.println("\033[35m¡¡¡EMPATE!!!\u001B[0m");
-                        System.out.println("El tablero esta lleno");
-                        System.out.println("======================");
-                    }
-                }
+                empate();
             }
+        }
+    }
+
+    public static void empate(){
+        if (jugadasMaximas == (Filas * Columnas) + 1) {
+            finJuego = true;
+            mostrarTablero(tablero);
+            System.out.println("======================");
+            System.out.println("\033[35m¡¡¡EMPATE!!!\u001B[0m");
+            System.out.println("El tablero esta lleno");
+            System.out.println("======================");
         }
     }
 }
